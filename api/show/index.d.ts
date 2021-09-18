4e58f116205238afb116ace92452210a7258f483
pengze123 <53901485+pengze123@users.noreply.github.com>
2021-09-18 11:12:19 +0800

    Update index.d.ts

--- api/show/index.d.ts
+++ api/show/index.d.ts
@@ -4,25 +4,30 @@ type IRawData = number
 
 // 统计数据类型，包含统计指标
 interface IStat {
-	[propName: string] : any
-	//
+	count:number,
+	sum:number,
+	mean:number,
+	median:number,
+	max:number,
+	min:number
+	
 }
 
 // 展示数据接口
 export interface IShowResponse {
 	// 0 为成功，其他为失败
-	code: number
+	code: number,
 	// 错误信息
-	message: string
+	message: string,
 	data: {
 		// 元数据
-		raw: IRawData[],
+		raw: IRawData[],    //以,分隔开的一组数
 		// 统计指标
-		stat: IStat[]
+		stat: IStat[]     //以,分隔开的键值对
 	}
 }
 
 export interface IShowRequest {
 	key: string,
-	ids: number[]
+	ids: number
 }
