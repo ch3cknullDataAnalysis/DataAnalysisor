683389f2f18b75566bf2c4e2b51cdf012d7c603d
ch3cknull <ch3cknull@outlook.com>
2021-10-08 10:38:11 +0800

    feat: add predict api

---
+++ api/predict/index.ts
@@ -0,0 +1,20 @@
+// 数据预测接口
+
+
+export interface IPredictResponse {
+	// 0 为成功，其他为失败
+	code: number,
+	// 错误信息
+	message: string,
+	data: {
+        result: string
+        num: number
+	}
+}
+
+
+export interface IPredictRequest {
+	key: string, // 传递的对应属性名称
+	cityNum: number//城市的id
+}
+
